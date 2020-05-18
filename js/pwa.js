609900ee97b73f9d57e276be132b134916a7b94b
medsearch <64500783+medsearch@users.noreply.github.com>
2020-05-18 04:15:18 +0000

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,11 +1,63 @@
-document.addEventListener('DOMContentLoaded', init, false);
-function init() {
-  if ('serviceWorker' in navigator) {
+const notificationButton = document.getElementById('enableNotifications');
+let swRegistration = null;
+
+function initializeApp() {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
+    //Register the service worker
     navigator.serviceWorker.register('./service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered -->', reg);
-      }, (err) => {
-        console.error('Service worker not registered -->', err);
+      .then(swReg => {
+        console.log('Service Worker is registered', swReg);
+        // We are storing the service worker, globally
+        swRegistration = swReg;
+        initializeUi();
+      })
+      .catch(error => {
+        console.error('Service Worker Error', error);
       });
+  } else {
+    console.warn('Push messaging is not supported');
+    notificationButton.textContent = 'Push Not Supported';
   }
 }
+
+function initializeUi() {
+  notificationButton.addEventListener('click', () => {
+    displayNotification();
+  });
+}
+
+function displayNotification() {
+  //Ask user if we show notifications
+  if (window.Notification && Notification.permission === 'granted') {
+    notification();
+  }
+  // If the user hasn't told whether he wants to be notified or not
+  // Note: because of Chrome, we cannot be sure the permission property
+  // is set, therefore it's unsafe to check for the "default" value.
+  else if (window.Notification && Notification.permission !== 'denied') {
+    Notification.requestPermission(status => {
+      if (status === 'granted') {
+        notification();
+      } else {
+        alert('You denied or dismissed permissions to notifications.');
+      }
+    });
+  } else {
+    // If the user refuses to get notified
+    alert(
+      'You denied permissions to notifications. Please go to your browser or phone setting to allow notifications.'
+    );
+  }
+}
+
+function notification() {
+  const options = {
+    body: 'Testing Our Notification',
+    icon: './../images/bell.png'
+  };
+  swRegistration.showNotification('PWA Notification!', options);
+}
+
+
