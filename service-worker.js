ab69f81f88465e57260c7a0da6afa42cfe0961ef
medsearch <64500783+medsearch@users.noreply.github.com>
2020-05-18 05:01:47 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -44,6 +44,7 @@ self.addEventListener('fetch', function(event) {
   })
 
 self.addEventListener('push', event => {
+  console.log(event);
     const dataJSON = event.data.json();
 
     const notificationOptions = {
